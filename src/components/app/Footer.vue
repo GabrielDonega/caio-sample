b3efa29f968e4ebb637a8719b93d7713528ecd46
Caio Kawasaki <caio@rilo.com.br>
2022-07-25 22:28:22 -0300

    wip

---
+++ src/components/app/Footer.vue
@@ -0,0 +1,196 @@
+<template>
+  <footer>
+    <div class="upper">
+      <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+          <div class="cell large-4">
+            <div class="about">
+              <div class="logo"/>
+              <div class="text">Nulla diam felis, malesuada ut erat quis, mattis dignissim justo. Cras cursus, lacus et
+                dignissim volutpat
+              </div>
+            </div>
+          </div>
+          <div class="cell large-8">
+            <div class="grid-x grid-margin-x">
+              <div class="cell large-4">
+                <div class="menu-group">
+                  <div class="title">Quick Links</div>
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">About Us</a>
+                    </li>
+                    <li>
+                      <a href="#">Services</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="cell large-4">
+                <div class="menu-group">
+                  <div class="title">Quick Links</div>
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">About Us</a>
+                    </li>
+                    <li>
+                      <a href="#">Services</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="cell large-4">
+                <div class="menu-group">
+                  <div class="title">Contact Us</div>
+                  <ul>
+                    <li>shield.info@gmail.com</li>
+                    <li>+91 - 99xxx 000xx</li>
+                  </ul>
+                </div>
+                <div class="menu-group">
+                  <div class="title">Follow Us</div>
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sub">
+      <div class="grid-container">
+        <div class="left">&copy; Copyright &copy; Sheild. All Rights Reserved.</div>
+        <div class="right">
+          <ul>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Tearms Condition</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+footer {
+  color: $darkBlue;
+
+  .upper {
+    background: $lightGray;
+    padding: 100px 0;
+
+    .about {
+      .logo {
+        background: $darkBlue;
+        margin-bottom: 40px;
+        width: 195px;
+        height: 50px;
+      }
+
+      .text {
+        color: rgba($darkBlue, .5);
+      }
+    }
+
+    .menu-group {
+      .title {
+        margin-bottom: 20px;
+        color: #000;
+        font-family: $ubuntu;
+        font-weight: 700;
+      }
+
+      ul {
+        li {
+          color: rgba($darkBlue, .5);
+
+          a {
+            color: rgba($darkBlue, .5);
+            transition: .4s;
+
+            &:hover {
+              color: $darkBlue;
+            }
+          }
+
+          &:not(:last-child) {
+            margin-bottom: 14px;
+          }
+        }
+      }
+
+      &:not(:last-child) {
+        margin-bottom: 30px;
+      }
+    }
+  }
+
+  .sub {
+    padding: 40px 0;
+    font-size: 13px;
+
+    .grid-container {
+      display: flex;
+      align-items: center;
+
+      .left {
+      }
+
+      .right {
+        margin-left: auto;
+
+        ul {
+          display: flex;
+          align-items: center;
+
+          li {
+            a {
+              color: $darkBlue;
+
+              &:hover {
+                text-decoration: underline;
+              }
+            }
+
+            &:not(:last-child) {
+              &:after {
+                display: inline-block;
+                content: '|';
+                padding: 0 10px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/app/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <header>
+    <div class="grid-container">
+      <div class="content">
+        <div class="left">
+          <div class="logo"/>
+        </div>
+        <div class="right">
+          <ul class="menu">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About Us</a>
+            </li>
+            <li>
+              <a href="">Services</a>
+            </li>
+            <li>
+              <a href="">Contact Us</a>
+            </li>
+            <li>
+              <common-button href="https://google.com" size="large" target="_blank">Login</common-button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import CommonButton from "@/components/common/Button";
+
+export default {
+  name: "AppHeader",
+  components: {
+    CommonButton
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 40px 0;
+  width: 100%;
+
+  .content {
+    display: flex;
+    align-items: center;
+
+    .left {
+      .logo {
+        background: $darkBlue;
+        width: 195px;
+        height: 50px;
+      }
+    }
+
+    .right {
+      margin-left: auto;
+
+      .menu {
+        display: flex;
+        align-items: center;
+
+        li {
+          a {
+            font-size: 18px;
+            font-weight: 500;
+          }
+
+          &:not(:last-child) {
+            margin-right: 40px;
+
+            a {
+              color: $darkBlue;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
